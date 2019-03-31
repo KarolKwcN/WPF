0cab84cb30615b054de7882a6fa6c7cb5c2edb0e
Karol Kwiecień <karol.kwiecien91@gmail.com>
2019-03-31 14:57:01 +0200

    zrobiona klasa stos

---
+++ zaj1/Stos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaj1
+{
+    public class Stos
+    {
+        List<String> stosik { get; set; }
+
+        public Stos()
+        {
+            this.stosik = new List<string>();
+        }
+
+        public void push (string item)
+        {
+            this.stosik.Add(item);
+        }
+
+        public void pop()
+        {
+            this.stosik.RemoveAt(stosik.Count -1);
+        }
+
+        public string top()
+        {
+            return this.stosik.Last();
+
+        }
+
+        public bool empty()
+        {
+            if(this.stosik.Count == 0)
+            {
+
+            }
+
+            return false;
+        }
+    }
+}
